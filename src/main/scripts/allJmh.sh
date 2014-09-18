7adde8907f0518181ba704065c97d62fd746c380
Arnaud Roger <arnaud.roger@gmail.com>
2014-09-18 17:18:40 +0100

    fix formatting

--- src/main/scripts/allJmh.sh
+++ src/main/scripts/allJmh.sh
@@ -8,12 +8,12 @@ if [[ -z "$1" ]]; then DB=mock; else DB=$1; fi
 
 
 function runJmh() {
-	java -jar $TARGET/benchmarks.jar $1 -foe true -gc true -rf csv -i 10 -wi 10 -f 10 -bm sample -v SILENT -jvmArgsAppend -Ddb=$DB -rff $TARGET/jmh.out
-	tail -n +2 $TARGET/jmh.out  | cut -d ',' -f 1,5,4,6 | sed -r 's/org.sfm.benchmark.jmh.([a-zA-Z-9]+).testQuery([0-9]+)"/\1",\2/g'
+	java -jar $TARGET/benchmarks.jar $1 -foe true -gc true -rf csv -i 1 -wi 1 -f 1 -bm sample -v SILENT -jvmArgsAppend -Ddb=$DB -rff $TARGET/jmh.out
+	tail -n +2 $TARGET/jmh.out  | cut -d ',' -f 1,4,5,6 | sed -r 's/org.sfm.benchmark.jmh.([a-zA-Z0-9]+).testQuery([0-9]+)"/\1",\2/g' | awk -F , '{ printf "%s,%04d,%s,%s,%s\n", $1, $2, $4, $5, $3 }'
 }
 
 
-echo '"Benchmark","QuerySize","Samples","Median","Error (99.9%)"'
+echo '"Benchmark","QuerySize","Median","Error (99.9%)","Samples"'
 runJmh PureJdbc
 runJmh SfmStatic
 runJmh SfmDynamic
