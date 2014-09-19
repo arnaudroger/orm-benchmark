5b35add2aab1dbd35c1cbfa30bb95e6914d65926
Arnaud Roger <arnaud.roger@gmail.com>
2014-09-19 16:45:40 +0100

    move ITERATION To a variable

--- src/main/scripts/allJmh.sh
+++ src/main/scripts/allJmh.sh
@@ -9,9 +9,10 @@ TARGET=$DIR/../../../target
 
 if [[ -z "$1" ]]; then DB=mock; else DB=$1; fi
 
+NB_ITERATION=10
 
 function runJmh() {
-	java -jar $TARGET/benchmarks.jar $1 -foe true -gc true -rf csv -i 10 -wi 10 -f 10 -bm sample -v SILENT -jvmArgsAppend -Ddb=$DB -rff $TARGET/jmh.out
+	java -jar $TARGET/benchmarks.jar $1 -foe true -gc true -rf csv -i $NB_ITERATION -wi $NB_ITERATION -f $NB_ITERATION -bm sample -v SILENT -jvmArgsAppend -Ddb=$DB -rff $TARGET/jmh.out
 	tail -n +2 $TARGET/jmh.out  | cut -d ',' -f 1,4,5,6 | sed -r 's/org.sfm.benchmark.jmh.([a-zA-Z0-9]+).testQuery([0-9]+)"/\1",\2/g' | awk -F , '{ printf "%s,%04d,%s,%s,%s\n", $1, $2, $4, $5, $3 }'
 }
 
