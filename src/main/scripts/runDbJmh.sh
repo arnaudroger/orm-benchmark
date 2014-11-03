abbb37d3bd85a1b03e5a98d65f6be202011f050d
Arnaud Roger (e19224) <Arnaud.Roger@cmegroup.com>
2014-11-03 21:23:30 +0000

    add version change

--- src/main/scripts/runDbJmh.sh
+++ src/main/scripts/runDbJmh.sh
@@ -8,7 +8,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TARGET=$DIR/../../../target
 
 DB=$1
+shift
 
+OUTFILE=$1
 shift
 
 NB_ITERATION=10
@@ -16,24 +18,14 @@ LIMIT=1,10,100,1000
 #LIMIT=10
 THREAD=1
 
-function runJmh() {
-    benchmark=$1
-    shift
-    benchmarkname=$1
-    shift
-	java -jar $TARGET/benchmarks.jar $benchmark -foe true -gc true -rf csv -i $NB_ITERATION -wi $NB_ITERATION -f $NB_ITERATION -v SILENT -bm sample -tu ns -p db=$DB -t $THREAD -rff $TARGET/jmh.out $*
-	tail -n +2 $TARGET/jmh.out  | tr -d '\r'  | awk -v name=$benchmarkname -F , '{ printf "%s,%s,%s,%s,%s\n", name, $9, $5, $6, $4  }'
-}
 
+java -jar $TARGET/benchmarks.jar\
+ -foe true -rf csv\
+ -i $NB_ITERATION\
+ -wi $NB_ITERATION\
+ -f $NB_ITERATION\
+ -bm avgt -tu ns -t $THREAD\
+ -p db=$DB\
+ -p limit=$LIMIT\
+ -rff $TARGET/$OUTFILE $*
 
-echo '"Benchmark","QuerySize","Median","Error (99.9%)","Samples"'
-runJmh PureJdbc PureJdbc -p limit=$LIMIT
-runJmh JdbcMapperStatic JdbcMapperStatic -p useAsm=true -p limit=$LIMIT
-runJmh JdbcMapperDynamic JdbcMapperDynamic -p useAsm=true -p limit=$LIMIT
-runJmh JdbcMapperDynamic JdbcMapperDynamicNoAsm -p useAsm=false -p limit=$LIMIT
-runJmh Roma Roma -p useAsm=false -p limit=$LIMIT
-runJmh Sql2O Sql2o -p limit=$LIMIT
-runJmh Jooq Jooq -p limit=$LIMIT
-runJmh Hibernate Hibernate -p limit=$LIMIT
-runJmh MyBatis MyBatis -p limit=$LIMIT
-runJmh RowMapper RowMapper -p limit=$LIMIT
