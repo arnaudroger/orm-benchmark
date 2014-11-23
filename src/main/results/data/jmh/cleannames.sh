8bacafd2729cfa599b5864a8269d6df4a34ce585
Arnaud Roger (e19224) <Arnaud.Roger@cmegroup.com>
2014-11-23 08:10:12 +0000

    change clean name script

---
+++ src/main/results/data/jmh/cleannames.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i.back 's/org.sfm.benchmark.//g' $1
+sed -i.back 's/csv.CsvBenchmark.test//g' $1
+sed -i.back 's/db.hibernate.//g' $1
+sed -i.back 's/db.jdbc.//g' $1
+sed -i.back 's/db.jooq.//g' $1
+sed -i.back 's/db.sfm.//g' $1
+sed -i.back 's/db.spring.//g' $1
+sed -i.back 's/db.sql2o.//g' $1
+sed -i.back 's/Benchmark.testQuery//g' $1
+sed -i.back 's/Benchmark//g' $1
