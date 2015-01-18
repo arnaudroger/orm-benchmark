ed03983530cf61eea22cc88faa567d73c6b4f5ab
Arnaud Roger <arnaud.roger@gmail.com>
2015-01-18 21:03:41 +0000

    new jooq results

--- src/main/scripts/runBenchmark.sh
+++ src/main/scripts/runBenchmark.sh
@@ -23,16 +23,16 @@ $SCRIPTS/cpuPreBenchmark.sh
 sudo update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java
 
 echo exec HSQLDB $BENCHMARK/data/jmh/${DATE}_1hsqldb.csv
-$SCRIPTS/runDbJmh.sh HSQLDB $BENCHMARK/data/jmh/${DATE}_java7_1hsqldb.csv
+$SCRIPTS/runDbJmh.sh HSQLDB $BENCHMARK/data/jmh/${DATE}_java7_1hsqldb.csv $*
 echo exec MYSQL  $BENCHMARK/data/jmh/${DATE}_2mysql.csv
-$SCRIPTS/runDbJmh.sh MYSQL $BENCHMARK/data/jmh/${DATE}_java7_2mysql.csv
+$SCRIPTS/runDbJmh.sh MYSQL $BENCHMARK/data/jmh/${DATE}_java7_2mysql.csv $*
 
 sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
 
 echo exec HSQLDB $BENCHMARK/data/jmh/${DATE}_1hsqldb.csv
-$SCRIPTS/runDbJmh.sh HSQLDB $BENCHMARK/data/jmh/${DATE}_java8_1hsqldb.csv
+$SCRIPTS/runDbJmh.sh HSQLDB $BENCHMARK/data/jmh/${DATE}_java8_1hsqldb.csv $*
 echo exec MYSQL  $BENCHMARK/data/jmh/${DATE}_2mysql.csv
-$SCRIPTS/runDbJmh.sh MYSQL $BENCHMARK/data/jmh/${DATE}_java8_2mysql.csv
+$SCRIPTS/runDbJmh.sh MYSQL $BENCHMARK/data/jmh/${DATE}_java8_2mysql.csv $*
 
 echo reset cpu freq
 $SCRIPTS/cpuPostBenchmark.sh
