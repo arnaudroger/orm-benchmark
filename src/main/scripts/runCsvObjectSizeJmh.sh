e6d28e7eec755b14297edd24228c918134991854
Arnaud Roger <arnaud.roger@gmail.com>
2015-03-24 22:03:40 +0000

    add object size csv bench

---
+++ src/main/scripts/runCsvObjectSizeJmh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+IFS=$'\n\t'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+TARGET=$DIR/../../../target
+
+OUTFILE=$1
+shift
+
+NB_ITERATION=10
+LIMIT=1000
+#LIMIT=10
+THREAD=1
+
+
+java -jar $TARGET/benchmarks.jar ObjectSizeGSCsvMapperBenchmark\
+ -foe true -rf csv\
+ -i $NB_ITERATION\
+ -wi $NB_ITERATION\
+ -f $NB_ITERATION\
+ -bm avgt -tu ns -t $THREAD\
+ -p limit=$LIMIT\
+ -rff $OUTFILE $*
+
